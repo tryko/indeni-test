bc3708e293ba974183ecee982353de8c25bfe741
tryko <itay83ko@gmail.com>
2019-09-24 16:01:54 +0300

    added firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 
 const config = {
-  apiKey: 'AIzaSyCRKO48D1XY9yUh41i04fu5Ms48FUelb6E',
-  authDomain: 'test-herolo.firebaseapp.com',
-  databaseURL: 'https://test-herolo.firebaseio.com',
-  projectId: 'test-herolo',
-  storageBucket: 'test-herolo.appspot.com',
-  messagingSenderId: '311405146173',
+  apiKey: "AIzaSyAEvH321zkHf8m_Syn0tiB4mRE4zR9sE-E",
+  authDomain: "indeni-test.firebaseapp.com",
+  databaseURL: "https://indeni-test.firebaseio.com",
+  projectId: "indeni-test",
+  storageBucket: "",
+  messagingSenderId: "1034221760481",
+  appId: "1:1034221760481:web:f2551af7838ccb2596aca0"
 };
 const firebaseProdactivityApp = firebase.initializeApp(config);
 
